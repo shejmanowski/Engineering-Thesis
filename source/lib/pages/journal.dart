624976d57d1a7bb88c42392a077e1cf03dea58d1
shejmanowski <shejmanowskii@gmail.com>
2023-11-13 16:03:01 +0100

    add basic quote page

--- source/lib/pages/journal.dart
+++ source/lib/pages/journal.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:good_mentality/pages/journal_entry_page.dart';
-import 'package:good_mentality/components/journal_entry_tile.dart';
 
 class JournalPage extends StatelessWidget {
   const JournalPage({super.key});
